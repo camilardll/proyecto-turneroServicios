c6dc82fe548944ed109b5e6c6406ff74ac430161
Camila Ruiz <camilardll@gmail.com>
2021-12-09 22:00:17 -0300

    Entrega Jquery

---
+++ clase12/js/script.js
@@ -0,0 +1,310 @@
+
+const turnos = [
+    {id: 1, servicio: "Plomería", nombre: "Pedro Pérez", img: "media/img/plomero_PedroP.jpg", yyyy: 2021, mm: "11", dd: "Jueves 25", hh: "10hs", estado: "Disponible"},
+    {id: 2, servicio: "Plomería", nombre: "Pedro Pérez", img: "media/img/plomero_PedroP.jpg", yyyy: 2021, mm: "11", dd: "Jueves 25", hh: "110hs", estado: "Disponible"},
+    {id: 3, servicio: "Gas", nombre: "Julia Andrada", img: "media/img/gasista_JuliaA.jpg", yyyy: 2021, mm: "11", dd: "Viernes 5", hh: "14hs", estado: "Disponible"},
+    {id: 4, servicio: "Gas", nombre: "Julia Andrada", img: "media/img/gasista_JuliaA.jpg", yyyy: 2021, mm: "11", dd: "Viernes 5", hh: "16hs", estado: "Disponible"},
+    {id: 5, servicio: "Gas", nombre: "Julia Andrada", img: "media/img/gasista_JuliaA.jpg", yyyy: 2021, mm: "11", dd: "Viernes 5", hh: "17hs", estado: "Disponible"},
+    {id: 6, servicio: "Electricidad", nombre: "Esteban Fuentes", img: "media/img/electricista_EstebanF.jpg", yyyy: 2021, mm: "11", dd: "Viernes 5", hh: "10hs", estado: "Disponible"}
+];
+
+const guardarLocal = (key, value) => { 
+    localStorage.setItem(key, value) 
+};
+
+guardarLocal("listaTurnos", JSON.stringify(turnos))
+
+
+const almacenados = JSON.parse(localStorage.getItem("listaTurnos"));
+
+const turnosDisponibles = []
+
+for(const objeto of almacenados) {
+    turnosDisponibles.push(objeto);
+}
+
+
+filtrarTodos ()
+
+function filtrarTodos () {
+
+    limpiarHtmlPrevio();
+
+    for (const turno of turnosDisponibles) {
+        let contenedor = document.createElement ("div")
+
+    
+        contenedor.innerHTML = `
+        <div class="card" style="width: 12rem;">
+        <img src="${turno.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${turno.nombre}</h5>
+        <h6 class="card-title">${turno.servicio}</h6>
+        <p class="card-text">${turno.dd}/${turno.mm}, ${turno.hh} </p>
+        <a class="btn btn-primary btn-agregar" id="${turno.id}">${turno.estado}</a>
+        </div>
+        </div>
+        `;
+    
+        //Se agrego class en el div para agregar css a futuro
+    
+       $("#calendario").append(contenedor);
+        
+    
+        
+    }
+
+    btnAgregar()
+}
+
+
+
+const filtroPlomeria = turnosDisponibles.filter (item => item.servicio === "Plomería")
+const filtroGas = turnosDisponibles.filter (item => item.servicio === "Gas")
+const filtroElectricidad = turnosDisponibles.filter (item => item.servicio === "Electricidad")
+
+
+
+$("#btnTodos").click ( () => 
+    filtrarTodos ()
+)
+
+
+$("#btnPlomeria").click ( () => 
+    filtrarServicioPlomeria()
+)
+
+$("#btnGas").click ( () => 
+    filtrarServicioGas()
+)
+
+$("#btnElectricidad").click ( () => 
+    filtrarServicioElectricidad()
+)
+
+
+
+function filtrarServicioPlomeria() {
+
+    limpiarHtmlPrevio();
+
+    for (const turno of filtroPlomeria) {
+        let contenedor = document.createElement ("div")
+    
+        contenedor.innerHTML = `
+        <div class="card" style="width: 12rem;">
+        <img src="${turno.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${turno.nombre}</h5>
+        <h6 class="card-title">${turno.servicio}</h6>
+        <p class="card-text">${turno.dd}/${turno.mm}, ${turno.hh} </p>
+        <a class="btn btn-primary btn-agregar" id="${turno.id}">${turno.estado}</a>
+        </div>
+        </div>
+        `;
+    
+        //Se agrego class en el div para agregar css a futuro
+         
+        
+        $("#calendario").append(contenedor);
+        
+    }
+
+    btnAgregar()
+}
+
+function filtrarServicioGas() {
+
+    limpiarHtmlPrevio();
+
+    for (const turno of filtroGas) {
+        let contenedor = document.createElement ("div")
+    
+        contenedor.innerHTML = `
+        <div class="card" style="width: 12rem;">
+        <img src="${turno.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${turno.nombre}</h5>
+        <h6 class="card-title">${turno.servicio}</h6>
+        <p class="card-text">${turno.dd}/${turno.mm}, ${turno.hh} </p>
+        <a class="btn btn-primary btn-agregar" id="${turno.id}">${turno.estado}</a>
+        </div>
+        </div>
+        `;
+    
+        //Se agrego class en el div para agregar css a futuro
+         
+        
+        $("#calendario").append(contenedor);
+        
+    }
+
+    btnAgregar()
+}
+
+function filtrarServicioElectricidad() {
+
+    limpiarHtmlPrevio();
+
+    for (const turno of filtroElectricidad) {
+        let contenedor = document.createElement ("div")
+    
+        contenedor.innerHTML = `
+        <div class="card" style="width: 12rem;">
+        <img src="${turno.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${turno.nombre}</h5>
+        <h6 class="card-title">${turno.servicio}</h6>
+        <p class="card-text">${turno.dd}/${turno.mm}, ${turno.hh} </p>
+        <a class="btn btn-primary btn-agregar" id="${turno.id}">${turno.estado}</a>
+        </div>
+        </div>
+        `;
+    
+        //Se agrego class en el div para agregar css a futuro
+         
+        
+        $("#calendario").append(contenedor);
+     
+    }
+
+    btnAgregar()
+}
+
+
+
+function limpiarHtmlPrevio() {
+    $("#calendario").html("");
+}
+
+
+
+function btnAgregar() {
+    let btn = $('.btn-agregar')
+    
+
+    for (const botones of btn) {
+        botones.addEventListener("click", () => {
+                agregarReserva(parseInt(botones.id))
+                
+            })
+        
+    }
+}
+
+
+
+
+
+const carritoReservas = []
+
+function agregarReserva (turnoID) {
+    let turnoReservado = turnosDisponibles.find ((item => item.id == turnoID));
+    carritoReservas.push(turnoReservado);
+
+    console.log(carritoReservas)
+    
+    
+    mostrarReservas();
+
+}
+
+   
+
+function mostrarReservas (){
+
+    limpiarHtmlPrevioReservas()
+        
+    for (const turno of carritoReservas) {
+        let contenedor2 = document.createElement ("div")
+    
+        contenedor2.innerHTML = `
+        <div class="card" style="width: 12rem;">
+        <img src="${turno.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${turno.nombre}</h5>
+        <h6 class="card-title">${turno.servicio}</h6>
+        <p class="card-text">${turno.dd}/${turno.mm}, ${turno.hh} </p>
+        <p class="card-text">Estado: Reservado</p>
+        <a class="btn btn-primary btn-eliminar" id="${turno.id}">Eliminar</a>
+        </div>
+        </div>
+        `;
+    
+        $("#misTurnos").append(contenedor2);
+               
+    }
+
+    btnEliminar()
+
+
+    function btnEliminar() {
+         
+        let btn = $('.btn-eliminar')
+        for (const botones of btn) {
+            $('.btn-eliminar').click ( () => 
+                    eliminarReserva(parseInt(botones.id))
+                    
+                )
+            
+        }
+    }
+
+
+
+    
+    
+    function eliminarReserva (turnoID) {
+        let turnoEliminado = carritoReservas.find ((item => item.id == turnoID));
+        carritoReservas.splice(turnoEliminado);
+    
+       
+        // turnosDisponibles[turnoID].reservar();
+        
+        
+        mostrarReservas();
+    
+    }
+    
+
+}
+
+function limpiarHtmlPrevioReservas() {
+    $("#misTurnos").html("");
+}
+
+
+
+
+
+
+
+// function btnEliminar() {
+//     let btn = document.getElementsByClassName('btn-eliminar')
+    
+
+//     for (const botones of btn) {
+//         botones.addEventListener("click", () => {
+//                 eliminarReserva(parseInt(botones.id))
+                
+//             })
+        
+//     }
+// }
+
+// function eliminarReserva (turnoID) {
+//     let turnoEliminado = carritoReservas.find ((item => item.id == turnoID));
+//     carritoReservas.splice(turnoEliminado);
+
+   
+//     // turnosDisponibles[turnoID].reservar();
+    
+    
+//     mostrarReservas();
+
+// }
+
+
+
+
+
